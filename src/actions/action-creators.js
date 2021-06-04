b54506278ff69633c6985bdbfe8cea50fe863423
MarcosPenna <marcoskpo_penpun@hotmail.com>
2021-06-04 19:58:13 -0300

    Adding redux-thunk. Modifying fetch action using axios

--- src/actions/action-creators.js
+++ src/actions/action-creators.js
@@ -1,4 +1,5 @@
 import * as types from './action-types'
+import axios from 'axios'
 
 export const actNewTodo = (name) => {
     return {
@@ -8,19 +9,27 @@ export const actNewTodo = (name) => {
 }
 
 export const actFetchTodos = () => {
-    return {
-        type: types.ACT_FETCH_TODO,
-        payload: [
-            {
-                id: 1,
-                name: "Estudiar React",
-                done: false
-            },
-            {
-                id: 2,
-                name: "Estudiar React Avanzado",
-                done: false
-            }
-        ]
+    // return {
+    //     type: types.ACT_FETCH_TODO,
+    //     payload: [
+    //         {
+    //             id: 1,
+    //             name: "Estudiar React",
+    //             done: false
+    //         },
+    //         {
+    //             id: 2,
+    //             name: "Estudiar React Avanzado",
+    //             done: false
+    //         }
+    //     ]
+    // }
+    return function(dispatch){
+        axios.get('http://localhost:3001/tareas').then((response) => {
+            dispatch({
+                type: types.ACT_FETCH_TODO,
+                payload: response.data
+            })
+        })
     }
 }
--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,6 @@
 import mainReducer from '../reducers/mainReducer'
 import {createStore, applyMiddleware} from 'redux'
+import thunk from 'redux-thunk'
 //import {logMiddleware} from '../middlewares/logMiddleware'
 //import { delayMiddleware } from '../middlewares/delayMiddleware';
 
@@ -9,6 +10,6 @@ const defaultData = {
     ]
 }
 
-const reduxConsole = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+//const reduxConsole = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
 
-export default createStore(mainReducer, defaultData, reduxConsole);
+export default createStore(mainReducer, defaultData, applyMiddleware(thunk));
